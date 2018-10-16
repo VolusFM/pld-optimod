35fd7068a3ff2dfd06f426b5a44aa78c5fc12429
FrancoisMontigny <francois.montigny@gmail.com>
2018-10-16 19:40:46 +0200

    default package and gitignore setup

---
+++ src/main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Montigny
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
