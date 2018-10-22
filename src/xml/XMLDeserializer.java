82cbba29361397b4bbc2c27eff586565df2a7569
Pomeg <44205023+Pomeg@users.noreply.github.com>
2018-10-22 09:27:44 +0200

    finish deserializer

--- src/xml/XMLDeserializer.java
+++ src/xml/XMLDeserializer.java
@@ -3,22 +3,19 @@ package xml;
 import java.io.File;
 import java.io.IOException;
 
+import javax.print.attribute.standard.Destination;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-
-/*import modele.Cercle;
-import modele.Plan;
-import modele.Point;
-import modele.PointFactory;
-import modele.Rectangle;*/
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import jdk.management.resource.internal.UnassignedContext;
+import model.Intersection;
+import model.Plan;
+import model.Section;
 
 public class XMLDeserializer {
 	public static void load(Plan plan) throws ParserConfigurationException, SAXException, IOException, XMLException {
@@ -35,19 +32,40 @@ public class XMLDeserializer {
 	private static void buildFromDOMXML(Element noeudDOMRacine, Plan plan) throws XMLException, NumberFormatException {
 		NodeList listIntersections = noeudDOMRacine.getElementsByTagName("noeud");
 		for (int i = 0; i < listIntersections.getLength(); i++) {
-			plan.addIntersection(createIntersection((Element) listIntersections.item(i)));
+			int id = Integer.parseInt(((Element) listIntersections.item(i)).getAttribute("id"));
+			Intersection intersection = createIntersection((Element) listIntersections.item(i));
+
+			plan.addIntersection(id, intersection);
 		}
-		NodeList listSections = noeudDOMRacine.getElementsByTagName("trocon");
+		NodeList listSections = noeudDOMRacine.getElementsByTagName("troncon");
 		for (int i = 0; i < listSections.getLength(); i++) {
-			plan.addSection(createSection((Element) listSections.item(i)));
+			Section section = createSection((Element) listSections.item(i));
+			
+			plan.addSection(section);
 		}
 	}
 	
 	private static Intersection createIntersection(Element element) throws XMLException {
 		double longitude = Integer.parseInt(element.getAttribute("longitude"));
 		double latitude = Integer.parseInt(element.getAttribute("latitude"));
-		int id = Integer.parseInt(element.getAttribute("id"));
-
-		return new Intersection(id, latitude, longitude);
+	
+		return new Intersection(latitude, longitude);
 	}
+	
+    private static Section createSection(Element elt) throws XMLException{ // TODO : naming
+    	int idDeparture = Integer.parseInt(elt.getAttribute("origine"));
+    	int idArrival = Integer.parseInt(elt.getAttribute("destination"));
+   		Intersection departure = plan.findIntersectionById(idDeparture); // XXX : is this OK ?
+   		Intersection arrival = plan.findIntersectionById(idArrival);
+   		
+   		String streetName = elt.getAttribute("nomRue");
+   		
+   		double length = Double.parseDouble(elt.getAttribute("longueur"));
+
+   		if (length <= 0) {
+   			throw new XMLException("Error when loading file : length of a section must be positive");
+   		}
+   		
+   		return new Section(departure, arrival, length, streetName);
+    }
 }
