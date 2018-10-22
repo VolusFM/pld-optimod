19c4c994a6d56780fc69b22a0608fa0516fdabd4
Piandow <pakoyaaki@gmail.com>
2018-10-22 12:11:39 +0200

    Updated model with Tour, TourCalculator and TourFactory

--- src/model/TourCalculator.java
+++ src/model/TourCalculator.java
@@ -1,11 +1,28 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TourCalculator {
 
-	public Tour createTour(List <Section> stepList, List <Delivery> deliveryPoints){
-		Tour currentTour = new Tour (stepList, deliveryPoints) ;
-		return currentTour;
+	private TourFactory tourFactory;
+	private Plan map;
+	private List<Delivery> deliveryRequest;
+    /** Instance unique pré-initialisée */
+	private static TourCalculator INSTANCE = new TourCalculator();
+	
+	private TourCalculator(){
+		tourFactory = TourFactory.getInstance();
+		deliveryRequest = new ArrayList<>();
+	}
+	
+	public static TourCalculator initialiseTourCalculator(Plan map, List<Delivery> deliveryRequest){
+		INSTANCE.deliveryRequest = deliveryRequest;
+		INSTANCE.map = map;
+		return INSTANCE;
+	}
+	
+	public static TourCalculator getInstance(){
+		return INSTANCE;
 	}
 }
