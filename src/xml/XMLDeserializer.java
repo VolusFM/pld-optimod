f1f2dad71b2dd66b0aa8865cd26779e44e98a020
FrancoisMontigny <francois.montigny@gmail.com>
2018-10-24 08:57:07 +0200

    Added deserialization methods for deliveries

--- src/xml/XMLDeserializer.java
+++ src/xml/XMLDeserializer.java
@@ -2,6 +2,7 @@ package xml;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -10,13 +11,28 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
+import model.Delivery;
 import model.Intersection;
 import model.Plan;
 import model.Section;
 
+/**
+ * 
+ * @author lsterner
+ *
+ */
 public class XMLDeserializer {
+	/**
+	 * Load a plan from an XML file.
+	 * @param plan
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws XMLException
+	 */
 	public static void load(Plan plan) throws ParserConfigurationException, SAXException, IOException, XMLException {
 		File xml = XMLFileOpener.getInstance().open();
 		DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
@@ -24,21 +40,52 @@ public class XMLDeserializer {
 		Element root = document.getDocumentElement();
 		if (root.getNodeName().equals("reseau")) {
 			buildFromDOMXML(root, plan);
-		} else
-			throw new XMLException("Unrecognized file format");
+		} else {
+			throw new XMLException("File's content doesn't match that of a plan");
+		}
 	}
 
-	private static void buildFromDOMXML(Element noeudDOMRacine, Plan plan) throws XMLException, NumberFormatException {
-		NodeList listIntersections = noeudDOMRacine.getElementsByTagName("noeud");
-		for (int i = 0; i < listIntersections.getLength(); i++) {
-			plan.addIntersection(createIntersection((Element) listIntersections.item(i)));
+	/**
+	 * 
+	 * @param rootNode
+	 * @param plan
+	 * @throws XMLException
+	 * @throws NumberFormatException
+	 */
+	private static void buildFromDOMXML(Element rootNode, Plan plan) throws XMLException, NumberFormatException {
+		NodeList intersections = rootNode.getElementsByTagName("noeud");
+		for (int i = 0; i < intersections.getLength(); i++) {
+			plan.addIntersection(createIntersection((Element) intersections.item(i)));
 		}
-		NodeList listSections = noeudDOMRacine.getElementsByTagName("troncon");
-		for (int i = 0; i < listSections.getLength(); i++) {
-			plan.addSection(createSection((Element) listSections.item(i), plan));
+		NodeList sections = rootNode.getElementsByTagName("troncon");
+		for (int i = 0; i < sections.getLength(); i++) {
+			plan.addSection(createSection((Element) sections.item(i), plan));
 		}
 	}
 	
+	public static void load(List<Delivery> deliveries) throws ParserConfigurationException, SAXException, IOException, XMLException {
+		File xml = XMLFileOpener.getInstance().open();
+		DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		Document document = docBuilder.parse(xml);
+		Element root = document.getDocumentElement();
+		if (root.getNodeName().equals("demandeDeLivraisons")) {
+			buildFromDOMXML(root, deliveries);
+		} else {
+			throw new XMLException("File's content doesn't match that of a deliveries request");
+		}
+	}
+	
+	private static void buildFromDOMXML(Element rootNode, List<Delivery> deliveries) throws XMLException, NumberFormatException {
+		Node depot = rootNode.getElementsByTagName("entrepot").item(0);
+		NodeList deliveriesNodes = rootNode.getElementsByTagName("livraison");
+	}
+	
+	/**
+	 * 
+	 * @param element
+	 * @return
+	 * @throws XMLException
+	 */
 	private static Intersection createIntersection(Element element) throws XMLException {
 		long id = Long.parseLong(element.getAttribute("id"));
 		double latitude = Double.parseDouble(element.getAttribute("latitude"));
@@ -48,6 +95,13 @@ public class XMLDeserializer {
 	}
 	
 
+	/**
+	 * 
+	 * @param elt
+	 * @param plan
+	 * @return
+	 * @throws XMLException
+	 */
     private static Section createSection(Element elt, Plan plan) throws XMLException{ // TODO : naming
     	long idDeparture = Long.parseLong(elt.getAttribute("origine"));
     	long idArrival = Long.parseLong(elt.getAttribute("destination"));
