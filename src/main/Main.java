fdf625cd5470917be3ec7b06c645f6bbae00f1ee
Pomeg <44205023+Pomeg@users.noreply.github.com>
2018-11-04 17:51:29 +0100

    add main

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+import model.Plan;
+import model.TourCalculator;
+import xml.XMLDeserializer;
+
+/**
+ * 
+ */
+
+/**
+ * @author Montigny
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		Plan plan = new Plan();
+		XMLDeserializer.load(plan);
+//		TourCalculator.init(plan, null, null);
+		TourCalculator.getInstance().setMap(plan);
+		
+		
+		XMLDeserializer.load(plan, TourCalculator.getInstance());
+		
+		TourCalculator.getInstance().calculateTours(1);
+	}
+
+}
