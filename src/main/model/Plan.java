cc1bace86c9e89f531fa92c5976ac640dee64563
FrancoisMontigny <francois.montigny@gmail.com>
2018-11-06 14:26:37 +0100

    chore: refactoring and documentation of Plan

--- src/main/model/Plan.java
+++ src/main/model/Plan.java
@@ -13,23 +13,44 @@ public class Plan {
 	private double HIGH = Double.MAX_VALUE;
 	private HashMap<Long, Intersection> graph;
 
+	/**
+	 * Add an intersection to the plan.
+	 * 
+	 * @param toAdd
+	 *            is the intersection to add.
+	 */
 	public void addIntersection(Intersection toAdd) {
 		graph.put(toAdd.getId(), toAdd);
 	}
 
+	/**
+	 * Add a section to the plan.
+	 * 
+	 * @param toAdd
+	 *            is the section to add.
+	 */
 	public void addSection(Section toAdd) {
 		long idIntersection = toAdd.getIdStartIntersection();
 		graph.get(idIntersection).addOutcomingSection(toAdd);
 	}
 
-	public Intersection getIntersectionById(long idDeparture) {
-		return graph.get(idDeparture);
+	/**
+	 * Retrieve an Intersection using its id.
+	 * @param id is the id of the Intersection we are looking for.
+	 * @return Intersection, the Intersection mapped by id in graph.
+	 */
+	public Intersection getIntersectionById(long id) {
+		return graph.get(id);
 	}
 
+	/**
+	 * Initialize plan.
+	 */
 	public Plan() {
 		graph = new HashMap<>();
 	}
 
+	@Deprecated
 	public void print() {
 		for (long key : graph.keySet()) {
 			System.out.println("got intersection " + key + " with sections :");
@@ -38,6 +59,10 @@ public class Plan {
 		}
 	}
 
+	/**
+	 * Getter for graph.
+	 * @return HashMap<Long, Intersection>, the graph of the plan.
+	 */
 	public HashMap<Long, Intersection> getGraph() {
 		return graph;
 	}
@@ -48,8 +73,8 @@ public class Plan {
 	 * reach.
 	 * 
 	 * @param sourceIntersection
-	 * @return a pair with as the first member distances, and as the second member
-	 *         predecessors.
+	 * @return a pair with as the first member distances, and as the second
+	 *         member predecessors.
 	 */
 	public Pair<HashMap<Long, Double>, HashMap<Long, Long>> Dijkstra(Intersection sourceIntersection) {
 		List<Long> settledId = new ArrayList<>();
@@ -68,7 +93,8 @@ public class Plan {
 
 		unSettledId.add(sourceIntersection.getId());
 
-		distances.put(sourceIntersection.getId(), 0.); // 0. because 0 is not a double
+		distances.put(sourceIntersection.getId(), 0.); // 0. because 0 is not a
+														// double
 
 		while (!unSettledId.isEmpty()) {
 			long idEvaluationIntersectionId = findIntersectionWithLowestDistance(distances, unSettledId);
@@ -89,15 +115,16 @@ public class Plan {
 			}
 		}
 
-		Pair<HashMap<Long, Double>, HashMap<Long, Long>> toReturn = new Pair<>(distances, predecessors);
-		return toReturn;
+		Pair<HashMap<Long, Double>, HashMap<Long, Long>> result = new Pair<>(distances, predecessors);
+		return result;
 	}
 
 	/**
-	 * find the lowest distances in distances for key in unSettledIntersectionId
+	 * Find the lowest distance in distances for key in unSettledId.
 	 * 
 	 * @param distances
 	 * @param unSettledIntersectionId
+	 * @return
 	 */
 	private long findIntersectionWithLowestDistance(HashMap<Long, Double> distances, List<Long> unSettledId) {
 		double lowestDistance = HIGH;
@@ -111,37 +138,49 @@ public class Plan {
 		}
 		return idIntersectionWithLowestDistance;
 	}
-	
-	
+
+	/**
+	 * Find the Intersection closest to where the user clicked.
+	 * @param latitude is the latitude of the point where the user clicked.
+	 * @param longitude is the longitude of the point where the user clicked.
+	 * @return Intersection, the closest Intersection.
+	 */
 	public Intersection findClosestIntersection(double latitude, double longitude) {
 		GeographicCoordinate reference = new GeographicCoordinate(latitude, longitude);
 		double minDistance;
 		double currentDistance;
 		Intersection closest;
 		Intersection current;
-		
+
 		Iterator<Intersection> intersectionIterator = graph.values().iterator();
-		closest = intersectionIterator.next();	
+		closest = intersectionIterator.next();
 		minDistance = distanceBetween(reference, new GeographicCoordinate(closest.getLat(), closest.getLon()));
 
 		while (intersectionIterator.hasNext()) {
 			current = intersectionIterator.next();
 			currentDistance = distanceBetween(reference, new GeographicCoordinate(current.getLat(), current.getLon()));
-			
+
 			if (currentDistance < minDistance) {
 				minDistance = currentDistance;
 				closest = current;
 			}
 		}
-		
+
 		System.out.println("Closest is id : " + closest.getId() + " with distance : " + minDistance);
-		
+
 		return closest;
 	}
 
-	
+	/**
+	 * Get the distance between two points on the plan.
+	 * @param coordinate contains the coordinates of the first point.
+	 * @param anotherCoordinate contains the coordinates of the second point. 
+	 * @return double, the distance between the two points.
+	 */
 	private double distanceBetween(GeographicCoordinate coordinate, GeographicCoordinate anotherCoordinate) {
-		return (Math.sqrt((coordinate.getLatitude() - anotherCoordinate.getLatitude()) * (coordinate.getLatitude() - anotherCoordinate.getLatitude()) + 
-				(coordinate.getLongitude() - anotherCoordinate.getLongitude()) * (coordinate.getLongitude() - anotherCoordinate.getLongitude())));
+		return (Math.sqrt((coordinate.getLatitude() - anotherCoordinate.getLatitude())
+				* (coordinate.getLatitude() - anotherCoordinate.getLatitude())
+				+ (coordinate.getLongitude() - anotherCoordinate.getLongitude())
+						* (coordinate.getLongitude() - anotherCoordinate.getLongitude())));
 	}
 }
