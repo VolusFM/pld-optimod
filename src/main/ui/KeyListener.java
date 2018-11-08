9c591103d5dbec69b761e5a6a943e95f8c5633ca
Pomeg <44205023+Pomeg@users.noreply.github.com>
2018-11-08 23:12:48 +0100

    add KeyListener for Plan

---
+++ src/main/ui/KeyListener.java
@@ -0,0 +1,31 @@
+package main.ui;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import main.controler.Controler;
+import main.model.Delivery;
+import main.model.ModelInterface;
+import main.model.TourCalculator;
+
+public class KeyListener extends KeyAdapter {
+
+	private Controler controler;
+
+	public KeyListener(Controler controler) {
+		this.controler = controler;
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_DELETE) {
+
+			Delivery delivery = ModelInterface.findCorrespondingDelivery(controler.getSelectedIntersection());
+			if (delivery != null) {
+				TourCalculator.getInstance().removeDeliveryFromTour(delivery, ModelInterface.getTourPlanning().get(0));
+				controler.getWindow().redraw();
+			}
+
+		}
+	}
+}
