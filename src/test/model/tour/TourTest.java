32069678bd7da516310aa8816d871dd238924d2f
Lisa Silvestri <pakoyaaki@gmail.com>
2018-11-14 10:38:45 +0100

    Finished test on Tour - coverage is only 30.6 % but main method will be tested in TourCalculator and TourFactory

--- src/test/model/tour/TourTest.java
+++ src/test/model/tour/TourTest.java
@@ -1,5 +1,7 @@
 package test.model.tour;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,9 +23,9 @@ public class TourTest {
 	public void setUp() {
 		steps = new ArrayList<Step>();
 		deliveries = new ArrayList<Delivery>();
-		Intersection depotIntersection = new Intersection(1,0,0);
+		Intersection depotIntersection = new Intersection(7,0,0);
 		depot = new Delivery(10,depotIntersection);
-		tour = new Tour(depot,steps,deliveries,1);
+		tour = new Tour(depot,steps,deliveries,2);
 	}
 	
 	@Test
@@ -31,6 +33,7 @@ public class TourTest {
 		Intersection intersection = new Intersection(2,2,2);
 		Delivery toAdd = new Delivery(10, intersection);
 		tour.addDeliveryAtIndex(toAdd,0);
+		assertEquals("Delivery wasnt correctly added", toAdd, tour.getDeliveryPoints().get(0));
 	}
 	
 	@Test(expected =  AssertionError.class)
@@ -43,5 +46,26 @@ public class TourTest {
 		Intersection intersection = new Intersection(2,2,2);
 		Delivery toAdd = new Delivery(10, intersection);
 		tour.addDeliveryAtIndex(toAdd, 15);
+	}
+	
+	@Test
+	public void testRemoveDelivery() {
+		Intersection intersection = new Intersection(5,2,2);
+		Delivery toAdd = new Delivery(10, intersection);
+		tour.addDeliveryAtIndex(toAdd,0);
+		tour.removeDelivery(toAdd);
+		assert(tour.getDeliveryPoints().size() == 1);
+	}
+	
+	@Test
+	public void testRemoveEmptyDelivery() {
+		tour.removeDelivery(null);
+	}
+	
+	@Test
+	public void testRemoveUnknownDelivery() {
+		Intersection intersection = new Intersection(5,2,2);
+		Delivery toRemove = new Delivery(10, intersection);
+		tour.removeDelivery(toRemove);
 	}
 }
